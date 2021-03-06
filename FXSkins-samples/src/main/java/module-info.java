de67a0601ec2f6ed5e3be745912c3abf06def0cc
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2021-03-06 13:32:16 +0000

    Fix samples (some of them stopped working when changing to modules)

--- FXSkins-samples/src/main/java/module-info.java
+++ FXSkins-samples/src/main/java/module-info.java
@@ -5,8 +5,12 @@ module com.pixelduke.samples.fxskins {
     requires javafx.swing;
     requires javafx.graphics;
 
+    requires org.controlsfx.controls;
+
     requires com.pixelduke.fxskins;
 
     exports com.pixelduke.samples.control.skin to javafx.graphics;
+    exports com.pixelduke.samples.control.skin.controllers to javafx.fxml;
+    opens com.pixelduke.samples.control.skin.controllers to javafx.fxml;
     opens com.pixelduke.samples.control.skin to javafx.fxml;
 }
